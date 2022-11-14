3fdeaf90bbb7bd56803ad2ca744ec54561dfc610
mercy <1248321243@qq.com>
2022-11-14 23:53:14 +0800

    完善react+ts的基础建设

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,7 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
+
+  resolve: {
+    extensions: [".tsx", ".jsx", ".ts", ".js"],
+  },
 
   module: {
     rules: [
@@ -13,7 +17,80 @@ module.exports = {
           // 将 JS 字符串生成为 style 节点
           "style-loader",
           // 将 CSS 转化成 CommonJS 模块
-          "css-loader",
+          {
+            loader: "css-loader",
+            options: {
+              // Enable CSS Modules features
+              modules: true,
+              importLoaders: 2,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+            },
+          },
+          // 将 PostCSS 编译成 CSS
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    // postcss-preset-env 包含 autoprefixer
+                    "postcss-preset-env",
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /.(scss|sass)$/,
+        include: path.resolve(__dirname, "../src"),
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          "style-loader",
+          // 将 CSS 转化成 CommonJS 模块
+          {
+            loader: "css-loader",
+            options: {
+              // Enable CSS Modules features
+              modules: true,
+              importLoaders: 2,
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+            },
+          },
+          // 将 PostCSS 编译成 CSS
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    // postcss-preset-env 包含 autoprefixer
+                    "postcss-preset-env",
+                  ],
+                ],
+              },
+            },
+          },
+          // 将 Sass 编译成 CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(js|ts|jsx|tsx)$/,
+        include: path.resolve(__dirname, "../src"),
+        use: [
+          {
+            loader: "esbuild-loader",
+            options: {
+              loader: "tsx",
+              target: "es2015",
+            },
+          },
         ],
       },
       {
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const logo = require("./assets/react-logo.png");
-const app = document.getElementById("app");
-const img = document.createElement("img");
-img.src = logo;
-app.appendChild(img);
