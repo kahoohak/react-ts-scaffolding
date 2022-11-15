561aec10a6d1c3feae4552934f96067bce9e8cc9
xingchen <xingchen.zhao@huimeihealth.com.cn>
2022-11-15 16:30:52 +0800

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const ComponentHome = () => {
+  return <div>home test router</div>;
+};
+
+const routers = [
+  {
+    path: "/",
+    text: "",
+    element: () => <ComponentHome />,
+  },
+];
 
 const App = () => {
-  return <div>this is app</div>;
+  return (
+    <div className="app">
+      <Routes>
+        {routers.map((item) => (
+          <Route key={item.path} path={item.path} element={item.element()}></Route>
+        ))}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
